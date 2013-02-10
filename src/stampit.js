310f962cc1b438d14668167e9a276afea323078b
Eric Elliott <eric@ericleads.com>
2013-02-10 09:39:07 -0800

    Got tests passing in IE8

--- src/stampit.js
+++ src/stampit.js
@@ -32,12 +32,26 @@ if (!Object.create) {
 
 // Shim .bind()
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function (context) {
-    var args = [].slice.call(arguments, 1);
-
-    return function () {
-      return this.apply(context, args);
-    }; 
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+ 
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+ 
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+ 
+    return fBound;
   };
 }
 
