f3f5fde31bdde699964693814133175569c4015c
Vasyl Boroviak <kore.sar@gmail.com>
2016-07-24 09:54:18 +1000

    New deep merging algorithm as in specifications

--- src/merge.js
+++ src/merge.js
@@ -1,30 +1,41 @@
-import isObject from './isObject';
 import slice from './slice';
 
+function isObjectLike(value) {
+  return !!value && typeof value == 'object';
+}
+
+/**
+ * The 'src' argument plays the command role.
+ * The returned values is always of the same type as the 'src'.
+ * @param dst
+ * @param src
+ * @param shallow
+ * @returns {*}
+ */
 function mergeOne(dst, src, shallow) {
   if (src === undefined) return dst;
-  const srcIdObject = isObject(src);
-  if (!srcIdObject) return src; // not a POJO, array, or function
 
+  // Deal with arrays the first because arrays are object-like.
   const dstIsArray = Array.isArray(dst);
   const srcIsArray = Array.isArray(src);
   if (srcIsArray) return (dstIsArray ? dst : []).concat(src);
-
-  if (shallow === undefined) shallow = false;
   if (dstIsArray) return mergeOne({}, src, shallow);
 
+  const srcIsObjectLike = isObjectLike(src);
+  if (!srcIsObjectLike) return src; // not a POJO or array or regexp overwrites
+
   const keys = Object.keys(src);
-  if (!dst) dst = {};
+  const returnValue = dst || {};
 
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
     const srcValue = src[key];
-    if (srcValue === undefined) continue;
-
-    dst[key] = shallow ? src[key] : mergeOne(dst[key], srcValue, shallow);
+    if (srcValue !== undefined) {
+      returnValue[key] = shallow ? src[key] : mergeOne(returnValue[key], srcValue, shallow);
+    }
   }
 
-  return dst;
+  return returnValue;
 }
 
 function mergeFew(dst, srcs, shallow) {
