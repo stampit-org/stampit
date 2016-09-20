26df86dcc1b4d97e2f6afb8648b7104eae8c2193
Vasyl Boroviak <kore.sar@gmail.com>
2016-09-20 22:47:36 +1000

    Build compose, isStamp, isComposable to the module root. Add tests for it.

--- test/import.js
+++ test/import.js
@@ -11,3 +11,39 @@ test('import is the same as require', (t) => {
 
   t.end();
 });
+
+test('can import the stampit/compose', t => {
+  const compose = require('../compose');
+  t.equal(typeof compose, 'function', 'Must be a function');
+
+  const stamp = compose();
+  t.equal(typeof stamp, 'function', 'Must be a function');
+  t.equal(typeof stamp.compose, 'function', 'Must be a function');
+
+  t.end();
+});
+
+test('can import the stampit/isStamp', t => {
+  const compose = require('../compose');
+  const isStamp = require('../isStamp');
+  t.equal(typeof isStamp, 'function', 'Must be a function');
+
+  const stamp = compose();
+  t.equal(isStamp(stamp), true, 'Must recognize stamps');
+  t.equal(isStamp(stamp.compose), false, 'Must return false for non stamps');
+
+  t.end();
+});
+
+test('can import the stampit/isComposable', t => {
+  const compose = require('../compose');
+  const isComposable = require('../isComposable');
+  t.equal(typeof isComposable, 'function', 'Must be a function');
+
+  const stamp = compose();
+  t.equal(isComposable(stamp), true, 'Must recognize stamps');
+  t.equal(isComposable(stamp.compose), true, 'Must recognize stamp descriptor');
+  t.equal(isComposable('a string'), false, 'Must return false for rubbish');
+
+  t.end();
+});
