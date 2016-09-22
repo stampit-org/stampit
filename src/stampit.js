5f4c2b83459871276aaaede85772aac43b16bc5e
Vasyl Boroviak <kore.sar@gmail.com>
2016-09-23 00:35:29 +1000

    Code shortening (#237)
    * Decrease the .min.js by 1KB (5.5KB->4.6KB)
    
    * Minor linting issue

--- src/stampit.js
+++ src/stampit.js
@@ -11,21 +11,17 @@ export {isStamp};
 
 const assign = Object.assign;
 
-/* Composition functions */
-
-function composeArgsCall(self, propName, action, args) {
-  const descriptor = {};
-  descriptor[propName] = action({}, ...args);
-  return ((self && self.compose) || stampit).call(self, descriptor);
+function createUtilityFunction(propName, action) {
+  return function composeUtil() {
+    const descriptor = {};
+    descriptor[propName] = action({}, ...arguments);
+    return ((this && this.compose) || stampit).call(this, descriptor);
+  };
 }
 
-export function methods(...args) {
-  return composeArgsCall(this, 'methods', assign, args);
-}
+export const methods = createUtilityFunction('methods', assign);
 
-export function properties(...args) {
-  return composeArgsCall(this, 'properties', assign, args);
-}
+export const properties = createUtilityFunction('properties', assign);
 export {properties as refs};
 export {properties as props};
 
@@ -36,38 +32,24 @@ export function initializers(...args) {
 }
 export {initializers as init};
 
-export function deepProperties(...args) {
-  return composeArgsCall(this, 'deepProperties', merge, args);
-}
+export const deepProperties = createUtilityFunction('deepProperties', merge);
 export {deepProperties as deepProps};
 
-export function staticProperties(...args) {
-  return composeArgsCall(this, 'staticProperties', assign, args);
-}
+export const staticProperties = createUtilityFunction('staticProperties', assign);
 export {staticProperties as statics};
 
-export function staticDeepProperties(...args) {
-  return composeArgsCall(this, 'staticDeepProperties', merge, args);
-}
+export const staticDeepProperties = createUtilityFunction('staticDeepProperties', merge);
 export {staticDeepProperties as deepStatics};
 
-export function configuration(...args) {
-  return composeArgsCall(this, 'configuration', assign, args);
-}
+export const configuration = createUtilityFunction('configuration', assign);
 export {configuration as conf};
 
-export function deepConfiguration(...args) {
-  return composeArgsCall(this, 'deepConfiguration', merge, args);
-}
+export const deepConfiguration = createUtilityFunction('deepConfiguration', merge);
 export {deepConfiguration as deepConf};
 
-export function propertyDescriptors(...args) {
-  return composeArgsCall(this, 'propertyDescriptors', assign, args);
-}
+export const propertyDescriptors = createUtilityFunction('propertyDescriptors', assign);
 
-export function staticPropertyDescriptors(...args) {
-  return composeArgsCall(this, 'staticPropertyDescriptors', assign, args);
-}
+export const staticPropertyDescriptors = createUtilityFunction('staticPropertyDescriptors', assign);
 
 const allUtilities = {
   methods,
